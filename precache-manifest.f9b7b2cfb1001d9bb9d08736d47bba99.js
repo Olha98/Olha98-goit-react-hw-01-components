58de89bfdc97b86c0ccaebbcd83471d157608588
Olha Hrymchak <forwork1177@gmail.com>
2020-08-12 20:14:37 +0300

    Updates

---
+++ precache-manifest.f9b7b2cfb1001d9bb9d08736d47bba99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f2c98dc827d53607545bf1521e5756",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8342a7ee035ffebb3ea1",
+    "url": "/goit-react-hw-01-components/static/css/main.7042bdc4.chunk.css"
+  },
+  {
+    "revision": "d7b5efed7bb164a3f647",
+    "url": "/goit-react-hw-01-components/static/js/2.fcf625f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fcf625f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8342a7ee035ffebb3ea1",
+    "url": "/goit-react-hw-01-components/static/js/main.0de6b583.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
